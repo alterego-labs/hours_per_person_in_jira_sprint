625db7f50ff87d0bfbedd18b8be1142f46f4ccea
Sergey Gernyak <sergeg1990@gmail.com>
2017-01-26 08:06:53 +0200

    Use `board` CLI option to preselect board

--- core/cli/options.rb
+++ core/cli/options.rb
@@ -1,4 +1,11 @@
 module CLI
   class Options < Struct.new(:board, :sprint)
+    def board_set?
+      board != nil && board != ""
+    end
+
+    def sprint_set?
+      sprint != nil && sprint != ""
+    end
   end
 end
